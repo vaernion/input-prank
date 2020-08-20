384d6da3a51ff8cfc709b5b018c16a653db995f4
vaernion <55745090+vaernion@users.noreply.github.com>
2020-08-20 16:51:04 +0000

    Deploying to gh-pages from  @ 1db29a3a15dba8a948e800b2a75cf5f4c7efaf21 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3361463116a3f08d07b26db02e159538.js"
+  "/input-prank/precache-manifest.d24372f6a47f3031974b6860fdcf85cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/input-prank/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
