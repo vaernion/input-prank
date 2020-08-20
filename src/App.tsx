1db29a3a15dba8a948e800b2a75cf5f4c7efaf21
Vaernion <vaporz@gmail.com>
2020-08-20 18:49:30 +0200

    basename...

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <ErrorBoundary>
         <Store>
-          <Router>
+          <Router basename={process.env.PUBLIC_URL}>
             <nav>
               <Menu />
             </nav>
